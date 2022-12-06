98a661cd67d486674d5131ac36144c3ca6f367ce
itanglishdev <chrishethertonweb@gmail.com>
2022-12-06 23:12:49 +0100

    added more read write functions

---
+++ 1-node-farm/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+
+// const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8')
+// console.log(textIn)
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
+// fs.writeFileSync('./starter/txt/output.txt', textOut)
+// console.log('File written!');
+
+fs.readFile('./starter/txt/start.txt', 'utf-8',(err, data1) => {
+    fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8',(err, data2) => {
+
+        console.log(data2)
+        fs.readFile(`./starter/txt/append.txt`, 'utf-8',(err, data3) => {
+            console.log(data3)
+            fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`,'utf-8', err => {
+                    console.log('Your file has been written!')
+            })
+        })
+    })
+    // console.log(data)
+})
+console.log('Will read file!');
