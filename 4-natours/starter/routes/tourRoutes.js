3b3641472e497803b27e53f21beda6a0653bed05
itanglishdev <chrishethertonweb@gmail.com>
2023-01-08 11:43:18 +0100

    added dotenv config and eslint config mess up

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -1,18 +1,20 @@
-const express = require('express')
+const express = require('express');
 
 // We are importing here 
-const tourController = require('./../controllers/tourController')
+const tourController = require('./../controllers/tourController');
 
 //  We could DESTRUCTURE the object also
 // const {getAllTours, createTour, getTour, updateTour, deleteTour} = require('./../controllers/tourController')
 
 const router = express.Router() 
 
+router.param('id', tourController.checkID)
+
 
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour)
+  .post(tourController.checkBody, tourController.createTour)
 
 router
   .route('/:id')
@@ -20,4 +22,4 @@ router
   .patch(tourController.updateTour)
   .delete(tourController.deleteTour)
 
-  module.exports = router
+  module.exports = router;
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,7 +1,10 @@
+const dotenv = require('dotenv')
+dotenv.config({path: './config.env'})
+
 const app = require('./app')
 
 //   CREATING A SERVER (PORT VARIABLE AND A LISTENER)
-const port = 3000
+const port = process.env.PORT ||  3000
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 })
