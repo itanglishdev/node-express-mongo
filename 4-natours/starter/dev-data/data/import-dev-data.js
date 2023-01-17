b7d43f1262188232ece59ebc44a1ad21a8ecb984
itanglishdev <chrishethertonweb@gmail.com>
2023-01-17 13:09:53 +0100

    added import dev data file

---
+++ 4-natours/starter/dev-data/data/import-dev-data.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const Tour = require('./../../models/tourModel')
+
+dotenv.config({path: './config.env'})
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>', 
+  process.env.DATABASE_PASSWORD)
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log(`DB connection succesful`))
+
+  // READ json FILE
+  const tours = JSON.parse(fs.readFileSync('tours-simple.json', 'utf-8'))
+
+  // IMPORTDATA INTO DB
+  const importData = async () => {
+    try {
+      await Tour.create(tours)
+      console.log('Data succesfully loaded!')
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  // DELETE ALL DATA FROM DB
