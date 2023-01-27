51fceb15b1e253ae686068ae406cceaa4d007d9e
itanglishdev <chrishethertonweb@gmail.com>
2023-01-27 17:01:23 +0100

    set basic filter

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -14,9 +14,23 @@ const Tour = require('./../models/tourModel')
 exports.getAllTours = async (req,res) => {
 
   try {
+    // BUILD QUERY
+    const queryObj = {...req.query}
+    const excludedFields = ['page', 'sort', 'limit', 'fields']
+    excludedFields.forEach(el => delete queryObj[el])
 
-    const tours = await Tour.find()
+    const query =  Tour.find(queryObj)
+
+    // const tours = await Tour.find()
+    // .where('duration')
+    // .equals(5)
+    // .where('difficulty')
+    // .equals('easy')
+
+    // EXECUTE QUERY
+    const tours = await query
   
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length, // when we send an array
